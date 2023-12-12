5bf5a7349a1dac93bae845f13214ced0fe601a8e
Pysthere <anggraahmad90@gmail.com>
2023-12-12 23:00:03 +0700

    master

---
+++ js/music-list.js
@@ -0,0 +1,117 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Ben Barlow - December",
+    artist: "Neck Deep",
+    img: "music-1",
+    src: "December"
+  },
+  {
+    name: "Ben Barlow – Wish You Were Here",
+    artist: "Neck Deep",
+    img: "music-2",
+    src: "Wish-You-Were-Here"
+  },
+  {
+    name: "Ben Barlow - Torn",
+    artist: "Neck Deep",
+    img: "music-3",
+    src: "Torn"
+  },
+  {
+    name: "Ben Barlow - In Bloom",
+    artist: "Neck Deep",
+    img: "music-4",
+    src: "in-bloom"
+  },
+  {
+    name: "Ben Barlow - A Part Of Me",
+    artist: "Neck Deep",
+    img: "music-5",
+    src: "a-part-of-me"
+  },
+  {
+    name: "Oliver Sykes - Drown",
+    artist: "Bring Me The Horizon",
+    img: "music-6",
+    src: "Drown"
+  },
+  {
+
+    name: "Harry Styles - It Will Rain",
+    artist: "One Direction",
+    img: "music-4",
+    src: "it-will-rain"
+  },
+  {
+    name: "Harry Styles - Night Changes",
+    artist: "One Direction",
+    img: "music-4",
+    src: "Night"
+  },
+  {
+    name: "Petter Gene Hernandez - Talking To The Moon",
+    artist: "Bruno Mars",
+    img: "music-4",
+    src: "talking-to"
+  },
+  {
+    name: "Petter Gene Hernandez - Versace On the Floor",
+    artist: "Bruno Mars",
+    img: "music-4",
+    src: "versace-on"
+  },
+  {
+    name: "Petter Gene Here - Thats Whats Likes",
+    artist: "Bruno Mars",
+    img: "music-4",
+    src: "thats"
+  },
+  {
+    name: "Taylor Alison - All to Well",
+    artist: "Taylor Swift",
+    img: "music-4",
+    src: "all-to"
+  },
+  {
+    name: "Taylor Alison - Enchanted",
+    artist: "Taylor Swift",
+    img: "music-4",
+    src: "enchanted"
+  },
+  {
+    name: "Poo bear - Hard Two Face Reality",
+    artist: "Poo bear & Justien Bieber",
+    img: "music-4",
+    src: "Hard"
+  },
+  {
+    name: "Andy Willsher - I Wanna be Yours",
+    artist: "Ta",
+    img: "music-4",
+    src: "i-wanna"
+  },
+  {
+    name: "Aziz Hendra - Somebody Pleasure",
+    artist: "Aziz Hendra",
+    img: "music-4",
+    src: "somebody"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
